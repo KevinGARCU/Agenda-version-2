5c8e4c83379ef3a233243bd47aad2f1a8bd7f231
KevinGARCU <42239520+KevinGARCU@users.noreply.github.com>
2018-10-15 11:11:57 -0500

    Add files via upload

---
+++ Agenda (1)/src (1)/agenda (1)/Agenda (1).java
@@ -0,0 +1,12 @@
+package agenda;
+
+import interfaz.InterfazAgenda;
+
+public class Agenda {
+
+    public static void main(String[] args) {
+        InterfazAgenda ia;
+        ia = new InterfazAgenda();
+    }
+
+}
